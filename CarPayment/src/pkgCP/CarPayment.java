a467bb43349d508d83f85f662c037f0ffd68e1a7
Shontel <shontelc@udel.edu>
2018-09-27 12:44:17 -0400

    initial commit

---
+++ CarPayment/src/pkgCP/CarPayment.java
@@ -0,0 +1,23 @@
+package pkgCP;
+
+public class CarPayment {
+	private int carCost;
+	private int downPayment;
+	private int lengthOfLoans; 
+	private double interestRate;
+
+	
+	
+	public double monthlyPayment(){
+		double monthlyPay =
+				
+	}
+		
+	public double totalInterest() {
+		double totalInter = 
+				
+	}
+		
+		
+	
+}
